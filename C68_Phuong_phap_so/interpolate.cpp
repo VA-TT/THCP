d2586bae20e5305cf3641b554410e7968ae4fc7c
LuHuyen <152854551+ArthasTheShibe@users.noreply.github.com>
2025-09-13 22:59:27 +0200

    interpolation

--- C68_Phuong_phap_so/interpolate.cpp
+++ C68_Phuong_phap_so/interpolate.cpp
@@ -1,26 +1,60 @@
 #include <iostream>
-#include <fstream>
+#include <array>
 #include <cmath>
 #include <iomanip>
-#include <vector>
 
-class Dual
+// Hàm tính đa thức cơ sở Lagrange l_i(x)
+template <std::size_t N>
+double basisPolynomial(std::size_t i,
+                       const std::array<double, N>& data_x,
+                       double x)
 {
-private:
-    double val{1.0};
-    double der{1.0};
-};
+    double li = 1.0;
+    double xi = data_x[i];
 
-double myFunc(double x)
-{
-    return x * x;
+    for (std::size_t j = 0; j < N; ++j)
+    {
+        if (j != i)
+        {
+            double xj = data_x[j];
+            li *= (x - xj) / (xi - xj);
+        }
+    }
+    return li;
 }
 
-double automaticDiff(std::function<double(double)> f, double x0)
+// Hàm nội suy Lagrange
+template <std::size_t N>
+double interpolatePolynomial(const std::array<double, N>& data_x,
+                             const std::array<double, N>& data_y,
+                             double x)
 {
+    double fx = 0.0;
+    for (std::size_t i = 0; i < N; ++i)
+    {
+        fx += data_y[i] * basisPolynomial(i, data_x, x);
+    }
+    return fx;
 }
+
 int main()
 {
+    constexpr std::size_t numberPoints = 7;
+
+    std::array<double, numberPoints> data_x
+        {0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+
+    std::array<double, numberPoints> data_y
+        {0.0, 0.8415, 0.9093, 0.1411, -0.7568, -0.9589, -0.2794};
+
+    std::cout << std::fixed << std::setprecision(6);
+
+    // Tính giá trị nội suy tại các điểm cách nhau 0.1
+    for (double x = 0.0; x <= 6.0; x += 0.1)
+    {
+        double fx = interpolatePolynomial(data_x, data_y, x);
+        std::cout << "P(" << x << ") = " << fx << '\n';
+    }
 
     return 0;
 }
